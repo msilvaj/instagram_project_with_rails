3973ee908ac738ba88535b98071681331bb432bb
msilvaj <miqueias.ufpi@gmail.com>
2021-02-17 17:56:19 -0300

    Configure actions: create, index and new, to Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_10_102120) do
+ActiveRecord::Schema.define(version: 2021_02_17_195136) do
 
   create_table "posts", force: :cascade do |t|
     t.string "caption"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
