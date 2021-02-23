46e0093fc77a29139cd2f73046b3d665ad9b9409
msilvaj <miqueias.ufpi@gmail.com>
2021-02-23 18:52:03 -0300

    Finish Devise configurations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :owned_post, only: [:edit, :update, :destroy]
   before_action :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
@@ -10,11 +11,15 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    #@post = Post.new
+    #add devise configurations
+    @post = current_user.posts.build
   end
 
   def create
-    if @post = Post.create(post_params)
+    @post = current_user.posts.build(post_params)
+    #if @post = Post.create(post_params)
+    if @post.save
       flash[:success] = "Your post has been created"
       redirect_to posts_path
     else
@@ -54,5 +59,12 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:image, :caption)
+  end
+
+  def owned_post
+    unless current_user == @post.user
+      flash[:alert] = "That post doesn't belong to you!"
+      redirect_to root_path
+    end
   end
 end
