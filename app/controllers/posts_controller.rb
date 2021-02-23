24e128b0cdb72c9a44501ca457894cea7a1b72bc
msilvaj <miqueias.ufpi@gmail.com>
2021-02-23 09:57:01 -0300

    Code refactor to follow DRY principle

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -17,11 +18,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to(post_path(@post))
   end
@@ -30,7 +29,16 @@ class PostsController < ApplicationController
     
   end
 
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:image, :caption)
