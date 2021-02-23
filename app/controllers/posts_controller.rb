f67484f1a5f73e516c4d350b766a745fc5a7ea77
msilvaj <miqueias.ufpi@gmail.com>
2021-02-23 08:17:29 -0300

    Add Edit and Update functions to Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,8 +16,14 @@ class PostsController < ApplicationController
     redirect_to posts_path
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def update
-    
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to(post_path(@post))
   end
   
   def delete
