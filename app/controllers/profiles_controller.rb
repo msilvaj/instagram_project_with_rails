d79379c095f07d6d4898364ecb9bf8c5d0634f72
msilvaj <miqueias.ufpi@gmail.com>
2021-03-11 18:21:57 -0300

    Remove debug items from profiles_controller.rb

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,8 +3,6 @@ class ProfilesController < ApplicationController
   def show
     @user = User.find_by(user_name: params[:user_name])
     @posts = User.find_by(user_name: params[:user_name]).posts.order('created_at DESC')
-    puts "os posts do mnalucasdc"
-    puts @posts
   end
 
   def edit
