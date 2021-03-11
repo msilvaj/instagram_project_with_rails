a7283e947dfa89ac37ee0d2e148b534a56afee30
msilvaj <miqueias.ufpi@gmail.com>
2021-03-11 06:43:38 -0300

    Fix jquery error to handle link View x Comments

--- app/assets/javascripts/loadMoreComments.js
+++ app/assets/javascripts/loadMoreComments.js
@@ -1,45 +1,9 @@
-var Append = {};
-Append.open = false;
-function ClickableCommentsLink() {
-    $('.more-comments').click(function () {
-        $(this).on('ajax:success', function (event, data, status, xhr) {
-            var postId = $(this).data("post-id");
-            $("#comments_" + postId).html(data);
-            $("#comments-paginator-" + postId).html("<a id='more-comments' data-post-id=" + postId + " data-type='html' data-remote='true' href='/posts/" + postId + "/comments>show more comments</a>");
-            Append.open = true;
-            Append.comment = true;
-            Append.link = false;
-        });
+$( document ).ready(function() {
+    $('.more-comments').click( function() {
+      $(this).on('ajax:success', function(event) {
+        var postId = $(this).data("post-id");
+        $("#comments_" + postId).html(event.detail[2].responseText);
+        $("#comments-paginator-" + postId).html("<a id='more-comments' data-post-id=" + postId + "data-type='html' data-remote='true' href='/posts/" + postId + "/comments>show more comments</a>");
+      });
     });
-}
-function DestroyComments() {
-    $('.delete-comment').click(function () {
-        Append.id = this;
-        Append.post_id = $(this).data("post-id");
-        Append.comment_count = $(this).data("value");
-    });
-}
-$(document).ready(function () {
-    ClickableCommentsLink();
-    DestroyComments();
-    $('.comment_content').click(function () {
-        Append.id = this;
-        Append.post_id = $(this).data("post-id");
-        if (Append.comment_count == undefined) {
-            Append.comment_count =
-                $(this).data("value");
-        }
-        if (Append.comment_count < 4) {
-            Append.comment = true; Append.link
-                = false;
-        }
-        else if (Append.comment_count == 4) {
-            Append.comment = false;
-            Append.link = true;
-        }
-        else if (Append.comment_count > 4) {
-            Append.comment = false;
-            Append.link = false;
-        }
-    });
-});
+  });
