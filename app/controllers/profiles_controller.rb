2b452f3b3826ed114f077c6110b8b1528095fac2
msilvaj <miqueias.ufpi@gmail.com>
2021-03-11 13:57:05 -0300

    Add Show action to profile and configure route

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,12 @@
 class ProfilesController < ApplicationController
+  
   def show
+    @user = User.find_by(user_name: params[:user_name])
     @posts = User.find_by(user_name: params[:user_name]).posts.order('created_at DESC')
     puts "os posts do mnalucasdc"
     puts @posts
   end
+
+  
+
 end
