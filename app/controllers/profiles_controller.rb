14406cd7ec9822bfd2a682d1a6b85b7cfcea3275
msilvaj <miqueias.ufpi@gmail.com>
2021-03-12 11:13:17 -0300

    DRY profiles_controller.rb

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,19 +1,17 @@
 class ProfilesController < ApplicationController
+  before_action :set_user
   before_action :owned_profile, only: [:edit, :update]
   before_action :authenticate_user!
 
 
   def show
-    @user = User.find_by(user_name: params[:user_name])
     @posts = User.find_by(user_name: params[:user_name]).posts.order('created_at DESC')
   end
 
   def edit
-    @user = User.find_by(user_name: params[:user_name])
   end
   
   def update
-    @user = User.find_by(user_name: params[:user_name])
     if @user.update(profile_params)
       flash[:success] = 'Your profile has been updated.'
       redirect_to profile_path(@user.user_name)
@@ -26,12 +24,15 @@ class ProfilesController < ApplicationController
 
   private
 
+  def set_user
+    @user = User.find_by(user_name: params[:user_name])
+  end
+
   def profile_params
     params.require(:user).permit(:avatar, :bio)
   end
 
   def owned_profile
-    @user = User.find_by(user_name: params[:user_name])
     unless current_user == @user
       flash[:alert] = "That profile doesn't belong to you!"
       redirect_to root_path
